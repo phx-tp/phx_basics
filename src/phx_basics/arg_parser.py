257849354df2b7964c0b1de622725eb81b116cfc
Mira Hlavacek <miroslav.hlavacek@phonexia.com>
2023-10-17 08:58:19 +0200

    add_argument proxy in default ArgParser

--- src/phx_basics/arg_parser.py
+++ src/phx_basics/arg_parser.py
@@ -10,7 +10,8 @@ from phx_basics.logging_tools import logging_format
 class ArgParser:
     """
     Create parser with debug and warning options and set logging. Parser can be modified as argparse.ArgumentParser via
-    'self.parser'
+    'self.parser'.
+    Most commonly used "add_argument" method can be used directly and will proxy args and kwargs to 'self.parser'.
     """
     def __init__(self, description: str = ""):
         self.parser = self._default_parser(description)
@@ -23,6 +24,10 @@ class ArgParser:
         logging_level.add_argument("--warning", action="store_true", help="Print only warning logging")
         parser.add_argument("--log", help="Log all into file", metavar="LOGFILE")
         return parser
+
+    def add_argument(self, *args, **kwargs):
+        """ Proxy to argparse's add_argument method """
+        self.parser.add_argument(*args, **kwargs)
 
     def __call__(self):
         args = self.parser.parse_args()
