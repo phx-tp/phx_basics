444bf7b5e33ca6b0c2283ac89f2471997e8b109b
Mira Hlavacek <miroslav.hlavacek@phonexia.com>
2025-02-24 22:11:51 +0100

    Added simple filter removing duplicate messages from logger

--- src/phx_basics/logging_tools.py
+++ src/phx_basics/logging_tools.py
@@ -1,2 +1,12 @@
 def logging_format():
     return '%(asctime)s %(levelname)s: %(message)s '
+
+
+class DuplicateFilter(object):
+    def __init__(self):
+        self.msgs = set()
+
+    def filter(self, record):
+        rv = record.msg not in self.msgs
+        self.msgs.add(record.msg)
+        return rv
